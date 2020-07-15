6e05035b3ea5f70defd935409531d29007054831
FallenWildCat <rmidhun3397@gmail.com>
2020-07-15 15:57:52 +0530

    '15-07-2020:15:57:51.11'

--- wp-content/uploads/custom-css-js/209071.js
+++ wp-content/uploads/custom-css-js/209071.js
@@ -1,7 +1,7 @@
 <!-- start Simple Custom CSS and JS -->
 <script type="text/javascript">
-const scriptURL = '<SCRIPT URL>'
-  const form = document.forms['submit-to-google-sheet']
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxYtiKbZEzM1I4mHtYnJ6A88pAmtqAsRyuiOAbhVA-g1_E-grpr/exec'
+  const form = document.forms['google-sheet']
 
   form.addEventListener('submit', e => {
     e.preventDefault()
