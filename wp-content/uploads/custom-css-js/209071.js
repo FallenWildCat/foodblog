cdc01cd12f5d192dae735d1b1ccb96d509cfae5b
Your-github-name <youremail@gmail.com>
2020-07-26 19:09:04 +0530

    committed on Sun, Jul 26, 2020  7:09:03 PM

--- wp-content/uploads/custom-css-js/209071.js
+++ wp-content/uploads/custom-css-js/209071.js
@@ -1,7 +1,7 @@
 <!-- start Simple Custom CSS and JS -->
 <script type="text/javascript">
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxYtiKbZEzM1I4mHtYnJ6A88pAmtqAsRyuiOAbhVA-g1_E-grpr/exec'
-  const form = document.forms['google-sheet']
+const form = document.forms['google-sheet']
 
   form.addEventListener('submit', e => {
     e.preventDefault()
