38e7babcef6efe4e6b64830d213ad9402bb33309
Your-github-name <youremail@gmail.com>
2020-07-28 17:26:25 +0530

    committed on Tue, Jul 28, 2020  5:26:25 PM

--- wp-content/uploads/custom-css-js/209272.js
+++ wp-content/uploads/custom-css-js/209272.js
@@ -1,7 +1,6 @@
 <!-- start Simple Custom CSS and JS -->
 <script type="text/javascript">
 src="https://cdn.jsdelivr.net/npm/sweetalert2@9"
-src="sweetalert2.all.min.js"
-src="https://cdn.jsdelivr.net/npm/promise-polyfill"
+
 </script>
 <!-- end Simple Custom CSS and JS -->
